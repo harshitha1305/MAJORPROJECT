41fbec67bf16e2985209069c41c1271de1db08b3
Harshitha <vharshitha1305@gmail.com>
2025-09-09 23:47:36 +0530

    Feature update

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,7 +5,7 @@ const ExpressError = require("../utils/ExpressError");
 const {listingSchema,reviewSchema} = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn,isReviewAuthor } = require("../middlewares.js");
+const { isLoggedIn,isReviewAuthor, isOwner } = require("../middlewares.js");
 const reviewController = require("../controllers/review.js");
 
 const validateReview = (req,res,next) =>{
@@ -22,6 +22,6 @@ const validateReview = (req,res,next) =>{
 router.post("/" ,validateReview,isLoggedIn, wrapAsync(reviewController.addReview));
 
 //delete review route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
+router.delete("/:reviewId",isLoggedIn,isOwner,isReviewAuthor,wrapAsync(reviewController.destroyReview));
 
 module.exports = router;

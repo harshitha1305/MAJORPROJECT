9c5d94d421f5c553ce1b919c56def3ba4143ab5c
Harshitha <vharshitha1305@gmail.com>
2025-09-10 04:51:25 +0530

    update

--- app.js
+++ app.js
@@ -85,6 +85,9 @@ app.use("/listings" , listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
